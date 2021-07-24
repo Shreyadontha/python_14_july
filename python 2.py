1c615619cf0647de3ad69cdae20c2c0ae475a984
Shreyadontha <87526495+Shreyadontha@users.noreply.github.com>
2021-07-24 17:00:01 +0530

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,15 @@
+name="shreya"
+print(name)
+rollno='77'
+print(rollno)
+a=int(input("enter the english marks:"))
+b=int(input("enter the telugu marks:"))
+c=int(input("enter the computer marks:"))
+d=int(input("enter the maths marks:"))
+e=int(input("enter the science marks:"))
+total=(a+b+c+d+e)
+print(total)
+percentage=(total/500)*100
+print(percentage)
+average=total/5
+print(average)
