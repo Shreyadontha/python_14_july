99c326f1cab43dc401c80d33bf2ff84ce8515242
Shreyadontha <87526495+Shreyadontha@users.noreply.github.com>
2021-07-24 17:01:24 +0530

    Add files via upload

---
+++ python6.py.py
@@ -0,0 +1,19 @@
+s={"name":"shreya","rollno":'77',"section":"MCA","email":"shreyadontha123@gmail.com","marks":'90'}
+keys =s.keys()
+print(keys)
+values =s.values()
+print(values)
+items =s.items()
+print(items)
+print(s)
+s.update({"address": "nrpt"})
+print(s)
+
+#set operation
+a={1,2,3,4,5,6}
+b={1,3,8,9,0,6}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+print(b.difference(a))
+print(sy
